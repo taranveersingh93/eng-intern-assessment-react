b981206c917497742012b1d3e86a7e4a3166ddbf
Taranveer Singh <taranveersingh93@gmail.com>
2024-01-21 19:27:54 -0500

    Add total time and timer-on states

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react'
+
+export default function App() {
+    const [totalTimeInMs, setTotalTimeInMs] = useState<number>(0);
+    const [timeRunning, setTimeRunning] = useState<boolean>(false);
+    return(
+        <div></div>
+    )
+}
